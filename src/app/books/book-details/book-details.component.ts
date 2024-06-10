de5151c41cad8ed0f7d0f289cc26622e267557db
Devon Pouw <devon_pouw@hotmail.com>
2024-06-10 20:40:12 +0200

    created book-details component

---
+++ src/app/books/book-details/book-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bf-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrl: './book-details.component.scss',
+})
+export class BookDetailsComponent {}
