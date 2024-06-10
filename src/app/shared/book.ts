8a4420c3e760e9247c1cf9080d4ed4a6f7975875
Devon Pouw <devon_pouw@hotmail.com>
2024-06-10 17:00:19 +0200

    created book interface

---
+++ src/app/shared/book.ts
@@ -0,0 +1,12 @@
+export interface Book {
+  cover_i: number;
+  has_fulltext: boolean;
+  edition_count: number;
+  title: string;
+  author_name: string[];
+  first_publish_year: number;
+  key: string;
+  ia: string[];
+  author_key: string[];
+  public_scan_b: boolean;
+}
