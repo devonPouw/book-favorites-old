137e2eeb39ed832622da110e039d1eaf14ca8879
Devon Pouw <devon_pouw@hotmail.com>
2024-06-12 18:58:16 +0200

    improved book-list

--- src/app/books/book-list-item/book-list-item.component.ts
+++ src/app/books/book-list-item/book-list-item.component.ts
@@ -1,11 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Book } from '../../shared/book';
 
 @Component({
   selector: 'bf-book-list-item',
   templateUrl: './book-list-item.component.html',
   styleUrl: './book-list-item.component.scss',
+  standalone: true,
 })
 export class BookListItemComponent {
-  @Input() book?: Book;
+  book = input.required<Book>();
+  getStarsArray(rating: number): number[] {
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 >= 0.5;
+    return Array.from(
+      { length: fullStars + (hasHalfStar ? 1 : 0) },
+      (_, i) => i + 1
+    );
+  }
+  parseRating(rating: number): number {
+    return Math.floor(rating);
+  }
 }
--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,31 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BookStoreService } from '../../shared/book-store.service';
-import { Book } from '../../shared/book';
-import { BookList } from '../../shared/book-list';
+import { BookListItemComponent } from '../book-list-item/book-list-item.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
-  selector: 'bf-book-list',
+  selector: 'bm-book-list',
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss',
+  standalone: true,
+  imports: [BookListItemComponent],
 })
 export class BookListComponent {
-  books: BookList = {
-    numFound: 0,
-    start: 0,
-    numFoundExact: false,
-    docs: [],
-    num_found: 0,
-    q: '',
-    offset: 0,
-  };
-  isLoading: boolean;
-
-  constructor(private service: BookStoreService) {
-    this.isLoading = true;
-    this.service.getAll().subscribe((books: BookList) => {
-      this.books = books;
-      this.isLoading = false;
-      console.log(this.books.docs);
-    });
-  }
+  books = toSignal(inject(BookStoreService).getInitialRandom());
 }
