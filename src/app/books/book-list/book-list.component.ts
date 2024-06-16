6e08ce32b70541337da68605c14cd345a16c4992
Devon Pouw <devon_pouw@hotmail.com>
2024-06-16 16:36:13 +0200

    working search filter with one input for now

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { BookStoreService } from '../../shared/book-store.service';
 import { BookListItemComponent } from '../book-list-item/book-list-item.component';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Subject, debounceTime, finalize, switchMap, tap } from 'rxjs';
+import { BookList } from '../../shared/book-list';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'bm-book-list',
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss',
   standalone: true,
-  imports: [BookListItemComponent],
+  imports: [BookListItemComponent, AsyncPipe],
 })
 export class BookListComponent {
-  books = toSignal(inject(BookStoreService).getInitialRandom());
+  private bookStoreService = inject(BookStoreService);
+
+  books = signal<Partial<BookList>>({ docs: [] });
+  private searchSubject$ = new Subject<string>();
+  isLoading = signal(false);
+  ngOnInit() {
+    this.isLoading.set(true);
+
+    this.bookStoreService
+      .getInitial()
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe((books) => {
+        this.books.set(books);
+      });
+
+    this.searchSubject$
+      .pipe(
+        debounceTime(400),
+        tap(() => this.isLoading.set(true)),
+        switchMap((query) => this.bookStoreService.searchBooks(query)),
+        tap(() => this.isLoading.set(false))
+      )
+      .subscribe((books) => {
+        this.books.set(books);
+      });
+  }
+
+  onInputChange(event: Event) {
+    this.searchSubject$.next((event.target as HTMLInputElement).value);
+  }
 }
