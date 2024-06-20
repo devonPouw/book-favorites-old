2dd2dfd7456d724667de2697f7d35ad8117213ee
Devon Pouw <devon_pouw@hotmail.com>
2024-06-20 17:41:10 +0200

    removed unnecessary interface fields so partial is not needed

--- src/app/shared/book-list.ts
+++ src/app/shared/book-list.ts
@@ -3,9 +3,6 @@ import { Book } from './book';
 export interface BookList {
   numFound: number;
   start: number;
-  numFoundExact: boolean;
   docs: Book[];
-  num_found: number;
-  q: string;
   offset: number;
 }
