ca17fddfe7237984ee6a16c43cafab4e4ad05db2
Devon Pouw <devon_pouw@hotmail.com>
2024-07-02 16:06:07 +0200

    changed home page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,6 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
     component: HomeComponent,
   },
   {
